b2c02118448469f8f5ca10eadafa5c6f8d1275c9
sharath <sharaths.kashyap@gmail.com>
2020-03-29 18:34:51 +0530

    Issue #0000:Creation of Chat Library Interface Elements

--- projects/chat-lib/src/lib/chat-lib.module.ts
+++ projects/chat-lib/src/lib/chat-lib.module.ts
@@ -1,22 +1,17 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; 
 import { CommonModule } from '@angular/common';
 import { ChatWindowComponent } from './chat-window/chat-window.component';
 import { ChatMessageListComponent } from './chat-message-list/chat-message-list.component';
 import { ChatMessageComponent } from './chat-message/chat-message.component';
 import { ChatMessageBottomBarComponent } from './chat-message-bottom-bar/chat-message-bottom-bar.component';
-import { ChatLibService } from './chat-lib.service';
 @NgModule({
   declarations: [ChatWindowComponent, ChatMessageListComponent, ChatMessageComponent, ChatMessageBottomBarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   exports: [ChatWindowComponent, ChatMessageListComponent, ChatMessageComponent, ChatMessageBottomBarComponent]
 })
 export class ChatLibModule { 
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: ChatLibModule,
-      providers: [ChatLibService]
-    };
-  }
 }
--- projects/chat-lib/src/public_api.ts
+++ projects/chat-lib/src/public_api.ts
@@ -1,6 +1,7 @@
 /*
  * Public API Surface of chat-lib
  */
-export * from './lib/chat-lib.module';
+
 export * from './lib/chat-lib.service';
+export * from './lib/chat-lib.module';
 
