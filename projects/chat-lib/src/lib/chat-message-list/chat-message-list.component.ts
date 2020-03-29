328f8595bc8253e76a89fef74e6de7e00b7e05d6
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-03-29 22:43:53 +0530

    Service integration

--- projects/chat-lib/src/lib/chat-message-list/chat-message-list.component.ts
+++ projects/chat-lib/src/lib/chat-message-list/chat-message-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ChatLibService } from '../chat-lib.service';
 @Component({
   selector: 'lib-chat-message-list',
   templateUrl: './chat-message-list.component.html',
@@ -7,31 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatMessageListComponent implements OnInit {
   public array = [
-    {
-      type: 'sent',
-      text: 'Hey how you are doing! '
-    },
-    {
-      type: 'recieved',
-      text: 'I am doing good'
-    },
-    {
-      type: 'recieved',
-      text: 'What help do you need!'
-    },
-    {
-      type: 'sent',
-      text: 'need help on COVID-19'
-    },
-    {
-      type: 'recieved',
-      text: 'SOme logn text here! asiudh haisu dhiaus hdiuahs iduahsi duahisud haisud hiaushdi uadi uahsiduha siudhai sudhiaus hdiuhaisudh aisu'
-    }
+    // {
+    //   type: 'sent',
+    //   text: 'Hey how you are doing! '
+    // },
+    // {
+    //   type: 'recieved',
+    //   text: 'I am doing good'
+    // },
+    // {
+    //   type: 'recieved',
+    //   text: 'What help do you need!'
+    // },
+    // {
+    //   type: 'sent',
+    //   text: 'need help on COVID-19'
+    // },
+    // {
+    //   type: 'recieved',
+    //   text: 'SOme logn text here! asiudh haisu dhiaus hdiuahs iduahsi duahisud haisud hiaushdi uadi uahsiduha siudhai sudhiaus hdiuhaisudh aisu'
+    // }
   ];
 
-  constructor() { }
+  constructor(public chatService: ChatLibService) { }
 
   ngOnInit() {
+    this.array = this.chatService.chatList;
+
   }
 
 }
