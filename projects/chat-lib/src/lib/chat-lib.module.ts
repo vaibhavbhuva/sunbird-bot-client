81de141c83f0b6f76aca1fd990fd9c8b748152f2
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-03-29 17:59:41 +0530

    Service changes

--- projects/chat-lib/src/lib/chat-lib.module.ts
+++ projects/chat-lib/src/lib/chat-lib.module.ts
@@ -4,12 +4,13 @@ import { ChatWindowComponent } from './chat-window/chat-window.component';
 import { ChatMessageListComponent } from './chat-message-list/chat-message-list.component';
 import { ChatMessageComponent } from './chat-message/chat-message.component';
 import { ChatMessageBottomBarComponent } from './chat-message-bottom-bar/chat-message-bottom-bar.component';
-
+import { ChatLibService } from './chat-lib.service';
 @NgModule({
   declarations: [ChatWindowComponent, ChatMessageListComponent, ChatMessageComponent, ChatMessageBottomBarComponent],
   imports: [
     CommonModule
   ],
+  providers : [ChatLibService],
   exports: [ChatWindowComponent, ChatMessageListComponent, ChatMessageComponent, ChatMessageBottomBarComponent]
 })
 export class ChatLibModule { }
