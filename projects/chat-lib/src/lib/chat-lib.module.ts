724b4be1d9daea71adc66a78ef5950f304ca6a29
subranil <subranil.saha@tarento.com>
2020-03-29 18:18:46 +0530

    added service for post api calls

--- projects/chat-lib/src/lib/chat-lib.module.ts
+++ projects/chat-lib/src/lib/chat-lib.module.ts
@@ -4,11 +4,16 @@ import { ChatWindowComponent } from './chat-window/chat-window.component';
 import { ChatMessageListComponent } from './chat-message-list/chat-message-list.component';
 import { ChatMessageComponent } from './chat-message/chat-message.component';
 import { ChatMessageBottomBarComponent } from './chat-message-bottom-bar/chat-message-bottom-bar.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [ChatLibComponent, ChatWindowComponent, ChatMessageListComponent, ChatMessageComponent, ChatMessageBottomBarComponent],
   imports: [
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  exports: [ChatLibComponent,ChatLibComponent, ChatWindowComponent, ChatMessageListComponent, ChatMessageComponent, ChatMessageBottomBarComponent]
+  exports: [ChatLibComponent, ChatLibComponent, ChatWindowComponent, ChatMessageListComponent,
+    ChatMessageComponent, ChatMessageBottomBarComponent]
 })
 export class ChatLibModule { }
--- projects/chat-lib/src/lib/chat-lib.service.ts
+++ projects/chat-lib/src/lib/chat-lib.service.ts
@@ -1,9 +1,60 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {ChatRequest} from '../model/request';
+import {catchError, map, mergeMap, scan} from 'rxjs/operators';
+import {ChatResponse} from '../model/response';
+import {Observable, of, Subject} from 'rxjs';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatLibService {
+  public requestStream: Subject<ChatRequest> = new Subject();
+  public responseStream: Observable<ChatResponse[]>;
+  public messageForm = new FormGroup({
+    message: new FormControl('', Validators.required)
+  });
+  constructor(
+    private http: HttpClient
+  ) {
+    this.responseStream = this.requestStream.pipe(
+      mergeMap((request) => {
+        return this.invokeApi(request);
+      }),
+      scan((acc: any, curr: any) => {
+        acc.push(curr);
+        return acc;
+      }, [])
+    );
+  }
 
-  constructor() { }
+  sendMessage() {
+    this.requestStream.next({
+      body: this.messageForm.get('message').value,
+      from: 'AC6436e7066283ef84c88a05392cc0fcd6'
+    });
+  }
+
+  invokeApi(request: ChatRequest): Observable<ChatResponse> {
+    return this.http.post('http://52.173.240.27:4000/bot', request, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      }),
+      responseType: 'text'
+    }).pipe(
+      map((response) => {
+        console.log(response);
+        return {
+          body: response
+        };
+      }),
+      catchError(err => {
+        console.error(err);
+        return of({
+          body: 'Unknown Error'
+        });
+      })
+    );
+  }
 }
