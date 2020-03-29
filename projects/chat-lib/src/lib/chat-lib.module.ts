b44d12d66b5a7769ae2c516c7da71c7573dc880b
sharath <sharaths.kashyap@gmail.com>
2020-03-29 16:25:18 +0530

    Issue #0000:Creation of Chat Library Interface Elements

--- projects/chat-lib/src/lib/chat-lib.module.ts
+++ projects/chat-lib/src/lib/chat-lib.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
-import { ChatLibComponent } from './chat-lib.component';
+import { CommonModule } from '@angular/common';
 import { ChatWindowComponent } from './chat-window/chat-window.component';
 import { ChatMessageListComponent } from './chat-message-list/chat-message-list.component';
 import { ChatMessageComponent } from './chat-message/chat-message.component';
 import { ChatMessageBottomBarComponent } from './chat-message-bottom-bar/chat-message-bottom-bar.component';
 
 @NgModule({
-  declarations: [ChatLibComponent, ChatWindowComponent, ChatMessageListComponent, ChatMessageComponent, ChatMessageBottomBarComponent],
+  declarations: [ChatWindowComponent, ChatMessageListComponent, ChatMessageComponent, ChatMessageBottomBarComponent],
   imports: [
+    CommonModule
   ],
-  exports: [ChatLibComponent,ChatLibComponent, ChatWindowComponent, ChatMessageListComponent, ChatMessageComponent, ChatMessageBottomBarComponent]
+  exports: [ChatWindowComponent, ChatMessageListComponent, ChatMessageComponent, ChatMessageBottomBarComponent]
 })
 export class ChatLibModule { }
--- projects/chat-lib/src/public_api.ts
+++ projects/chat-lib/src/public_api.ts
@@ -3,5 +3,4 @@
  */
 
 export * from './lib/chat-lib.service';
-export * from './lib/chat-lib.component';
 export * from './lib/chat-lib.module';
