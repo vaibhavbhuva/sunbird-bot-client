de40b126d73beeb43b96ea2172816ab43b63c5bf
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-04-03 16:05:01 +0530

    Issue #SB-0000 ChatBot PluginUpdate

--- projects/chat-lib/src/lib/chat-message-list/chat-message-list.component.ts
+++ projects/chat-lib/src/lib/chat-message-list/chat-message-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, AfterViewChecked, ViewChild, ElementRef } from '@angular/core';
 import { ChatLibService } from '../chat-lib.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject} from 'rxjs';
 @Component({
   selector: 'lib-chat-message-list',
   templateUrl: './chat-message-list.component.html',
@@ -9,33 +11,26 @@ export class ChatMessageListComponent implements OnInit, AfterViewChecked {
   @ViewChild('msgScrollToBottom') private msgScrollToBottom: ElementRef;
 
   public array = [
-    // {
-    //   type: 'sent',
-    //   text: 'Hey how you are doing! '
-    // },
-    // {
-    //   type: 'recieved',
-    //   text: 'I am doing good'
-    // },
-    // {
-    //   type: 'recieved',
-    //   text: 'What help do you need!'
-    // },
-    // {
-    //   type: 'sent',
-    //   text: 'need help on COVID-19'
-    // },
-    // {
-    //   type: 'recieved',
-    //   text: 'SOme logn text here! asiudh haisu dhiaus hdiuahs iduahsi duahisud haisud hiaushdi uadi uahsiduha siudhai sudhiaus hdiuhaisudh aisu'
-    // }
   ];
-
+  public unsubscribe$ = new Subject<void>();
   constructor(public chatService: ChatLibService) { }
 
   ngOnInit() {
     this.array = this.chatService.chatList;
-
+    if (this.array.length === 0 ) {
+;      const req = {
+        data: {
+          Body: "Hello"
+          }
+        }
+      this.chatService.chatpost(req).pipe(takeUntil(this.unsubscribe$)).subscribe(data => {
+        console.log(data)
+        this.chatService.chatListPush('recieved', data.text)
+      },err => {
+        console.log(err)
+        this.chatService.chatListPush('recieved', err.error.text)
+      });
+    }
   }
 
   ngAfterViewChecked() {
