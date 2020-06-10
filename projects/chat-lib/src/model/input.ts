646d0878c7d18df10df170437882bb2c253bcfea
ajinkya.ghodake <ajinkya.ghodake@tarento.com>
2020-06-10 15:29:52 +0530

    accepting input as object and set default params

---
+++ projects/chat-lib/src/model/input.ts
@@ -0,0 +1,11 @@
+export class  InputValues {
+    collapsed: boolean;
+    did: string;
+    userId: string;
+    channel: string;
+    appId: string;
+    title?: string;
+    imageUrl?: string; 
+    chatbotUrl: string;
+    context?: string;
+}
