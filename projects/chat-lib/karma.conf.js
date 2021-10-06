d0df56b7019f22491c52857e14511e1879b88666
Nik720 <nikunj.beladiya@tarento.com>
2021-10-06 12:26:37 +0530

    Issue #SB-26392 fix: updated karma config

--- projects/chat-lib/karma.conf.js
+++ projects/chat-lib/karma.conf.js
@@ -8,24 +8,54 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
+      require('karma-mocha-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    browserNoActivityTimeout: 100000,
     client: {
+      jasmine: {
+        timeoutInterval: 300000
+      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage'),
-      reports: ['html', 'lcovonly'],
+      dir: require('path').join(__dirname, '../../coverage'), reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    
+    reporters: ['mocha', 'coverage-istanbul'],
+    mochaReporter: {
+      symbols: {
+        success: '+',
+        info: '#',
+        warning: '!',
+        error: 'x'
+      }
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 60000,
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--no-sandbox',
+          '--headless',
+          '--disable-gpu',
+          // Without a remote debugging port, Google Chrome exits immediately.
+          '--remote-debugging-port=9222',
+          '--proxy-server=direct://',
+          '--proxy-bypass-list=*',
+          '--max_old_space_size=4096'
+        ]
+      }
+    },
     singleRun: false
-  });
+  })
 };
