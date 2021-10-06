a110a8226a27d6a8f09ba55b68fed33356b87a40
Nik720 <nikunj.beladiya@tarento.com>
2021-10-06 15:11:40 +0530

    Issue #SB-26392 fix: Added basic unit test cases

--- projects/chat-lib/src/lib/chat-window/chat-window.component.spec.ts
+++ projects/chat-lib/src/lib/chat-window/chat-window.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ChatWindowComponent } from './chat-window.component';
 
@@ -8,7 +10,9 @@ describe('ChatWindowComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChatWindowComponent ]
+      imports: [HttpClientTestingModule],
+      declarations: [ ChatWindowComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -16,6 +20,13 @@ describe('ChatWindowComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ChatWindowComponent);
     component = fixture.componentInstance;
+    component.inputValues = {
+      did: 'did123',
+      userId: 'U123',
+      channel: '123',
+      appId: 'sunbird.bot',
+      chatbotUrl: 'https://staging.sunbird.com'
+    }
     fixture.detectChanges();
   });
 
