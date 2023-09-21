e10a72dba3ffcd9fdcbc0281d0c8e2fdde4ba4fa
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2023-09-21 16:03:21 +0530

    Issue feat: dynamic UUID functionality added

--- projects/sunbird-bot-app/src/app/app.component.ts
+++ projects/sunbird-bot-app/src/app/app.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { environment } from './../environments/environment';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = environment.botName;
-  botConfig = {
-    header: environment.botName,
-    chatbotUrl: environment.apiUrl,
-    uuid: environment.uuid
+  botConfig: any;
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events
+         .subscribe((event) => {
+           if (event instanceof NavigationEnd) {
+             let uuid = this.route.snapshot.queryParamMap.get('uuid');
+              this.botConfig = {
+                header: environment.botName,
+                chatbotUrl: environment.apiUrl,
+                uuid: uuid ?  uuid : environment.uuid
+              }
+           }
+       }); 
+
   }
+
 }
--- projects/sunbird-bot-app/src/app/app.module.ts
+++ projects/sunbird-bot-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {ChatLibModule, ChatLibService} from 'chat-lib';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ChatLibModule
+    ChatLibModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
